5ecbef2e8c6a4ff124e443e3cad659c695c861cc
Marko Praakli <markopraakli@gmail.com>
2024-12-05 13:03:28 +0200

    wifi upgrade

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,51 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get thereHasBeenAProblem => 'There has been a problem. Please restart the app.';
+
+  @override
+  String get noDevicesFound => 'No Qilowatt devices found.';
+
+  @override
+  String get tryAgain => 'Try again.';
+
+  @override
+  String get failedToConnectTo => 'Failed to connect to ';
+
+  @override
+  String get connectionTookTooLong => 'connection took too long';
+
+  @override
+  String get platformNotSupported => 'Platform not supported.';
+
+  @override
+  String get locationPermissionsRequired => 'Location permissions required.';
+
+  @override
+  String get locationPermissionsDenied => 'Location permissions denied.';
+
+  @override
+  String get locationAccuracyPermissionsDenied => 'Location upgrade accuracy permissions denied.';
+
+  @override
+  String get locationServicesDisabled => 'Location services disabled.';
+
+  @override
+  String get wifiScanFailed => 'WiFi scan failed.';
+
+  @override
+  String get exception => 'Exception';
+
+  @override
+  String get pleaseEnableWifi => 'Please enable WiFi and try again.';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get restart => 'Restart';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_et.dart
@@ -0,0 +1,51 @@
+import 'app_localizations.dart';
+
+/// The translations for Estonian (`et`).
+class AppLocalizationsEt extends AppLocalizations {
+  AppLocalizationsEt([String locale = 'et']) : super(locale);
+
+  @override
+  String get thereHasBeenAProblem => 'Tekkis probleem. Palun taaskäivitage rakendus.';
+
+  @override
+  String get noDevicesFound => 'Qilowatt seadmeid ei leitud.';
+
+  @override
+  String get tryAgain => 'Proovige uuesti.';
+
+  @override
+  String get failedToConnectTo => 'Ühenduse loomine ebaõnnestus: ';
+
+  @override
+  String get connectionTookTooLong => 'ühenduse loomine võttis liiga kaua aega';
+
+  @override
+  String get platformNotSupported => 'Platvormi ei toetata.';
+
+  @override
+  String get locationPermissionsRequired => 'Asukoha kasutus pole lubatud.';
+
+  @override
+  String get locationPermissionsDenied => 'Asukoha kasutus pole keelatud.';
+
+  @override
+  String get locationAccuracyPermissionsDenied => 'Täpne asukoha kasutus on keelatud.';
+
+  @override
+  String get locationServicesDisabled => 'Asukohateave välja lülitatud.';
+
+  @override
+  String get wifiScanFailed => 'Seadmete otsimine ebaõnnestus.';
+
+  @override
+  String get exception => 'Viga';
+
+  @override
+  String get pleaseEnableWifi => 'Palun lülitage WiFi sisse ja proovige uuesti.';
+
+  @override
+  String get search => 'Otsi';
+
+  @override
+  String get restart => 'Taaskäivita';
+}
